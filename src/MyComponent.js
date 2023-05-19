7082020bd939ba8b753806af46e3f2f99fb996af
NguyenTrungDong <dongnguyen10245@gmail.com>
2023-05-20 02:31:02 +0700

    add onchange age

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -2,31 +2,49 @@ import React, { Fragment } from 'react';
 
 class MyComponent extends React.Component {
     state = {
-        name: '',
-    }
-
+      name: '',
+      age: '',
+    };
+  
     handleOnChangeName = (event) => {
-        this.setState({
-            name: event.target.value
-        });
-    }
-
+      this.setState({
+        name: event.target.value,
+      });
+    };
+  
+    handleOnChangeAge = (event) => {
+      this.setState({
+        age: event.target.value,
+      });
+    };
+  
     render() {
-        return (
-            <>
-                <div className='FirstName'>
-                  enter name:   <input
-                        value={this.state.name}
-                        type='text'
-                        onChange={this.handleOnChangeName}
-                    /><br/>
-                    My name is {this.state.name}
-                </div>
-                <div></div>
-            </>
-        );
+      return (
+        <>
+          <div className='name'>
+            enter name:
+            <input
+              value={this.state.name}
+              type='text'
+              onChange={this.handleOnChangeName}
+            />
+            <br />
+            My name's: {this.state.name}
+          </div>
+          <div className='age'>
+            enter age:
+            <input
+              value={this.state.age}
+              type='number'
+              onChange={this.handleOnChangeAge}
+            />
+            <br />
+            Age: {this.state.age}
+          </div>
+        </>
+      );
     }
-}
-
+  }
+  
 
 export default MyComponent;
